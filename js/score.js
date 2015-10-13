5aba22a163ca772fa7e27fd89bbf7a48d068ea6d
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-10-13 09:18:51 -0600

    fe

--- js/score.js
+++ js/score.js
@@ -5,10 +5,19 @@
  * Created by gabedriggs1 on 10/13/15.
  */
 var score;
-function teamScore()
+function quaffle1()
 {
     score =0;
     var team1 = document.getElementById("hScore").innerHTML = score +=10;
-    var team2 = document.getElementById("gScore").innerHTML = score +=10;
     score += 10;
+}
+function quaffle2(){
+score=0;
+    var team2 = document.getElementById("gScore").innerHTML = score +=10;
+    score+=10;
+}
+
+function snitch(){
+    score+=150;
+    if
 }
