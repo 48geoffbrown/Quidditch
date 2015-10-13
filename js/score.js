2256bcfb23a424b4d7cd7b55d84b70bb4864d9ba
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-10-13 09:11:41 -0600

    committing

---
+++ js/score.js
@@ -0,0 +1,14 @@
+/**
+ * Created by gabedriggs1 on 10/13/15.
+ */
+/**
+ * Created by gabedriggs1 on 10/13/15.
+ */
+var score;
+function teamScore()
+{
+    score =0;
+    var team1 = document.getElementById("hScore").innerHTML = score +=10;
+    var team2 = document.getElementById("gScore").innerHTML = score +=10;
+    score += 10;
+}
