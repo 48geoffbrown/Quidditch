6f3e9a2bc5bc056d89132839469ba3142ea347a9
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-10-13 09:50:49 -0600

    js upload

--- js/score.js
+++ js/score.js
@@ -4,20 +4,37 @@
 /**
  * Created by gabedriggs1 on 10/13/15.
  */
-var score;
-function quaffle1()
-{
-    score =0;
-    var team1 = document.getElementById("hScore").innerHTML = score +=10;
-    score += 10;
-}
-function quaffle2(){
-score=0;
-    var team2 = document.getElementById("gScore").innerHTML = score +=10;
-    score+=10;
-}
+var team1Score = 0;
+var team2Score = 0;
+function quaffle(team){
+    if (team == "1") {
+        team1Score += 10;
+        document.getElementById("team1").innerHTML = team1Score;
+    }
+    else {
+        team2Score += 10;
+        document.getElementById("team2").innerHTML = team2Score;
 
-function snitch(){
-    score+=150;
-    if
+    }
+}
+function snitch(team){
+    if (team == "1") {
+        team1Score += 30;
+        document.getElementById("team1").innerHTML = team1Score;
+        whoWon();
+    }
+    else {
+        team2Score += 30;
+        document.getElementById("team2").innerHTML = team2Score;
+        whoWon();
+
+    }
+}
+function whoWon(){
+    if (team1Score > team2Score){
+        alert("Team 1 wins!");
+    }
+    else {
+        alert("Team 2 Wins!");
+    }
 }
